82e5c081b4e18e9d56816b4993cca36cd4b9fdb7
bujhmt <bujhmt99999@gmail.com>
2020-10-17 19:09:56 +0300

    join to simple module

--- CUI/cui.py
+++ CUI/cui.py
@@ -2,37 +2,91 @@
 
 import os
 import sys
-import console 
-import menuTree as tree
 
+#console utils:
+def printBold(str):
+    BOLD = '\33[7m'
+    CEND = '\033[0m'
+    print(BOLD + str + CEND)
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def readChar():
+    import tty
+    import termios
+    tty.setcbreak(sys.stdin)
+    x = sys.stdin.read(1)[0]
+    return x
+
+def getch():
+    if (os.name == 'nt'):
+        import msvcrt
+        return msvcrt.getch()
+    else:
+        import tty
+        import termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+#Tree Node 
+class Node(object):
+
+    def __init__(self, title, on_press):
+        self.title = title
+        self.on_press = on_press
+        self.childs = []
+        self.root = None
+
+
+    def append(self, *args):
+        try:
+            if len(args) == 1 and isinstance(args[0], Node):
+                args[0].root = self
+                self.childs.append(args[0])
+            if len(args) == 2 and isinstance(args[0], str):
+                newNode = Node(args[0], args[1])
+                newNode.root = self
+                self.childs.append(newNode)
+        except Exception as err:
+            print("Error! ", err)
+
+#CUI class
 class CUI(object):
     #support:
-    __EXIT_NODE = tree.Node("EXIT", lambda: exit(1))
-    __EMPTY_NODE = tree.Node("", lambda: 0)
+    __EXIT_NODE = Node("EXIT", lambda: exit(1))
+    __EMPTY_NODE = Node("", lambda: 0)
 
     #private fields:
     __currentNode = __EMPTY_NODE
     __currentPos = 1
 
     def __init__(self, mainMenuTitle = 'Main menu'):
-        self.root = tree.Node(mainMenuTitle, lambda: None)
+        self.root = Node(mainMenuTitle, lambda: None)
         self.__currentNode = self.root
 
     def __print(self):
-        console.clear()
+        clear()
         #custom items
         print(f'-------{self.__currentNode.title}-------')
         for i in range(len(self.__currentNode.childs)):
             if i == self.__currentPos - 1:
-                console.printBold(f'[{self.__currentNode.childs[i].title}]')
+                printBold(f'[{self.__currentNode.childs[i].title}]')
             else:
                 print(f'[{self.__currentNode.childs[i].title}]')
 
     def __inputController(self):
         if os.name == 'nt':
-            return console.getch()
+            return getch()
         else:
-            return  console.readChar()
+            return  readChar()
 
     def __stepController(self, char):
         upperLimit: int = len(self.__currentNode.childs)
@@ -65,7 +119,7 @@ class CUI(object):
             self.__currentNode.append(title, lambda: 0)
 
     def addMenu(self, title):
-        newNode = tree.Node(title, lambda: self.__goToCurrentNode())
+        newNode = Node(title, lambda: self.__goToCurrentNode())
         newNode.append("Return to prev Menu", lambda: self.__goToParent())
         self.__currentNode.append(newNode)
         self.__currentNode = newNode
@@ -73,4 +127,3 @@ class CUI(object):
     def finishMenu(self):
         if self.__currentNode.root != None:
             self.__goToParent()
-
